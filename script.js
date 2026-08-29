127cf540633940e63f79d030d016fbdcdacd2b0b
Davin Tandy <dav.tandy@gmail.com>
2026-08-29 16:17:34 +0700

    Create function to calculate with operators

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const display = document.getElementById("display");
 const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+
+let firstNum = null;
+let currentOperator = null;
+let operatorPressed = false;
+
 
 function updateDisplay(event) {
     const number = event.target.textContent;
+
+    if (operatorPressed) {
+        display.value = "";
+        operatorPressed = false;
+    }
 
     if (number !== "." || !display.value.includes(".")) {
         display.value += event.target.textContent;
@@ -32,14 +43,38 @@ function operate(num1, num2, operator) {
     if (operator === "-") {
         return subtract(num1, num2);
     }
-    if (operator === "*") {
+    if (operator === "×") {
         return multiply(num1, num2);
     }
-    if (operator === "/") {
+    if (operator === "÷") {
         return divide(num1, num2);
     }
 }
 
+function handleOperator(event) {
+    const selectedOperator = event.target.textContent;
+    const currentNum = parseFloat(display.value);
+
+    if (isNaN(currentNum)) {
+        return
+    }
+
+    if (firstNum === null) {
+        firstNum = currentNum;
+    } else if (currentOperator) {
+        const result = operate(firstNum, currentNum, currentOperator);
+        display.value = result;
+        firstNum = result;
+    }
+
+    currentOperator = selectedOperator;
+    operatorPressed = true;
+}
+
 numbers.forEach(number => {
     number.addEventListener("click", updateDisplay);
+})
+
+operators.forEach(operator => {
+    operator.addEventListener("click", handleOperator);
 })
